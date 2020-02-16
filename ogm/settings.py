69fa0d9932022e0beba55117bcf39247b1a244a0
Mr. Konsta <panfilov2507@gmail.com>
2020-02-16 13:04:40 +0300

    последняя рабочая сборка

--- ogm/settings.py
+++ ogm/settings.py
@@ -104,7 +104,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
-LANGUAGE_CODE = 'ru-RU'
+LANGUAGE_CODE = 'ru'
 
 TIME_ZONE = 'Europe/Moscow'
 
